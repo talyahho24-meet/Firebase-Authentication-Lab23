fcbc7bda93d8dc44f4de7470f94a1d410a391d71
talyahho24-meet <139688490+talyahho24-meet@users.noreply.github.com>
2023-07-23 16:24:16 +0300

    dfghjkl

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {"apiKey": "AIzaSyDmQLDdOvWTq8GyIHH-ztMxio5IARiR-ww",
+  "authDomain": "feivel-ef716.firebaseapp.com",
+  "projectId": "feivel-ef716",
+  "storageBucket": "feivel-ef716.appspot.com",
+  "messagingSenderId": "480399213624",
+  "appId": "1:480399213624:web:0da0df2dbe4ce3a8ed12cd",
+  "measurementId": "G-5PNVEHC4VW"}
+
+  firebase = pyrebase.initialize_app(config)
+  auth = firebase.auth()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
